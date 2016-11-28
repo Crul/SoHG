c6c948f2f7e5d98876d2d096849d19c559a2d99f
Crul <rauljavier.vila@gmail.com>
2016-11-28 17:35:37 +0100

    Add Menu

---
+++ SoHG-unity/Assets/MenuAgg/Menu.cs
@@ -0,0 +1,26 @@
+﻿using Sohg.CrossCutting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Sohg.MenuAgg
+{
+    public class Menu : BaseComponent
+    {
+        [SerializeField]
+        private Button playButton;
+        [SerializeField]
+        private Button aboutButton;
+        [SerializeField]
+        private GameObject aboutPanel;
+        [SerializeField]
+        private Button closeAboutButton;
+
+        public void Awake()
+        {
+            playButton.onClick.AddListener(() => SceneManager.LoadScene("SohgGame"));
+            aboutButton.onClick.AddListener(() => aboutPanel.SetActive(true));
+            closeAboutButton.onClick.AddListener(() => aboutPanel.SetActive(false));
+        }
+    }
+}
