ce9b9b46b7c50d1682df54f149abdc9721d1f7ca
Crul <rauljavier.vila@gmail.com>
2016-12-09 20:44:55 +0100

    Add FPSDisplay

---
+++ SoHG-unity/Assets/CrossCutting/FPSDisplay.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class FPSDisplay : MonoBehaviour
+{
+    float deltaTime = 0.0f;
+
+    void Update()
+    {
+        deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+    }
+
+    void OnGUI()
+    {
+        int w = Screen.width, h = Screen.height;
+
+        GUIStyle style = new GUIStyle();
+
+        Rect rect = new Rect(0, 14 * h / 15, w, h / 50);
+        style.alignment = TextAnchor.UpperLeft;
+        style.fontSize = h / 15;
+        style.normal.textColor = new Color(0.5f, 1f, 0.0f, 1.0f);
+        float msec = deltaTime * 1000.0f;
+        float fps = 1.0f / deltaTime;
+        string text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
+        GUI.Label(rect, text, style);
+    }
+}
